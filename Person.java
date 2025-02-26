af6a76741ab84f8869854d320c6fcc02d99639fc
abinaya2006466175 <abinayasivakumar22@gmail.com>
2025-02-26 15:13:26 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,18 @@
+class Person
+{
+public String name="Abinaya";
+public int age=20;
+public void introduce()
+{
+System.out.print("my name is  " +name+ "  and my age is "+age);
+}
+
+public static void main(String args[])
+{
+Person p1=new Person();
+p1.introduce();
+}
+}
+
+
+
